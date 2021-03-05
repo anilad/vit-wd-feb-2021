36bc5690c4b38af221c091df0f59c06c1aaaf97a
Anilad <dalinadao@Dalinas-MacBook-Pro-2.local>
2021-03-04 18:43:36 -0800

    completed week2

--- week-2/day-4/exercise.js
+++ week-2/day-4/exercise.js
@@ -1,21 +1,28 @@
 function decrement() {
-  // access the DOM element associated with the h1
-  var h1Element = "CHANGE ME";
-  var numberText = Number(h1Element.innerText);
-  numberText++;
-  h1Element.innerText = numberText;
+    // access the DOM element associated with the h1
+    var h1Element = document.getElementById('grabMe');
+    var numberText = Number(h1Element.innerText);
+    if (numberText > 0) {
+        numberText--;
+
+    }
+    h1Element.innerText = numberText;
 }
 
 function increment() {
-  // write code here
+    // write code here
+    var h1Element = document.getElementById('grabMe');
+    var numberText = Number(h1Element.innerText);
+    numberText++;
+    h1Element.innerText = numberText;
 }
 
 // Done early? Try to add a blocker that doesnt go below 0 and above 10!
 
 function randomize() {
-  var x = Math.floor(Math.random() * 256);
-  var y = Math.floor(Math.random() * 256);
-  var z = Math.floor(Math.random() * 256);
-  var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-  document.body.style.background = bgColor;
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = bgColor;
 }
